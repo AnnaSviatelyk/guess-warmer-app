3fb4cc5be60967ffa27f466b8c47807debba65df
AnnaSv <anna.sviatelyk@gmail.com>
2020-07-20 13:44:53 +0400

    Updates

---
+++ precache-manifest.eea74de5a34f685928aa66cde2d3e3de.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddeecfbf58e657226c58e9ae6cb7a20c",
+    "url": "/guess-warmer-app/index.html"
+  },
+  {
+    "revision": "40baf1fa793c20bd6f8f",
+    "url": "/guess-warmer-app/static/css/main.62b5ae69.chunk.css"
+  },
+  {
+    "revision": "b799d3445cba287369cb",
+    "url": "/guess-warmer-app/static/js/2.62025a7a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/guess-warmer-app/static/js/2.62025a7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40baf1fa793c20bd6f8f",
+    "url": "/guess-warmer-app/static/js/main.f1090436.chunk.js"
+  },
+  {
+    "revision": "ca349e9179ebd2302051",
+    "url": "/guess-warmer-app/static/js/runtime-main.9a87e352.js"
+  },
+  {
+    "revision": "ff816a6ba864e3823b9be2dd0cc4d103",
+    "url": "/guess-warmer-app/static/media/Clear.ff816a6b.svg"
+  },
+  {
+    "revision": "bfd87d49d8ecc61be52aaac75c7dc289",
+    "url": "/guess-warmer-app/static/media/Clouds.bfd87d49.svg"
+  },
+  {
+    "revision": "741ef3bece7a86bbdd14f758279f2e3c",
+    "url": "/guess-warmer-app/static/media/Drizzle.741ef3be.svg"
+  },
+  {
+    "revision": "7085c5674a526de35556a520f7efd11f",
+    "url": "/guess-warmer-app/static/media/Other.7085c567.svg"
+  },
+  {
+    "revision": "741ef3bece7a86bbdd14f758279f2e3c",
+    "url": "/guess-warmer-app/static/media/Rain.741ef3be.svg"
+  },
+  {
+    "revision": "6c2481420ee983c66486cf12edd19adc",
+    "url": "/guess-warmer-app/static/media/Snow.6c248142.svg"
+  },
+  {
+    "revision": "6ca4d2828ca703b8d9bef59ec67c9c80",
+    "url": "/guess-warmer-app/static/media/ThunderStorm.6ca4d282.svg"
+  },
+  {
+    "revision": "abd95cbf600259c4a8cde6be281c66f3",
+    "url": "/guess-warmer-app/static/media/Tornado.abd95cbf.svg"
+  }
+]);
