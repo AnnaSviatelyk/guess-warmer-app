8b5f554b77f3f5a983163628838b9dd15536b334
AnnaSv <anna.sviatelyk@gmail.com>
2020-07-21 16:06:10 +0400

    uploads

--- src/components/HistoryPage/HistoryPage.js
+++ src/components/HistoryPage/HistoryPage.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './HistoryPage.scss'
 import CitiesOptions from '../CitiesOptions/CitiesOptions'
 import PropTypes from 'prop-types'
+import image from '../../assests/groot.svg'
 
 const HistoryPage = ({ results }) => {
     const allAnswers = results.map(cur => {
@@ -9,11 +10,21 @@ const HistoryPage = ({ results }) => {
         return <CitiesOptions cities={cur.cities} selectedAnswerId={cur.selectedAnswerId} key={cur.cities[0].id} />
     })
 
-    return (
+    const content = results.length ? (
         <div className='HistoryPage'>
             <h2 className="HistoryPage__heading">Here are your answers</h2>
             {allAnswers}
         </div>
+    ) : (
+            <div className='HistoryPage'>
+                <h2 className="HistoryPage__heading">You have no answers yet</h2>
+                <img className="HistoryPage__image" src={image} />
+
+            </div>
+        )
+
+    return (
+        content
 
     )
 
